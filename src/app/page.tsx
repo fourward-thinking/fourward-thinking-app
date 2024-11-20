078435d68d0df2809e5ad7f69516bf36f6e3a6a2
Kaenaokalani Sylva <kaena.sylva@gmail.com>
2024-11-19 18:48:48 -1000

    issue-08
    completed landing page mockup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,31 @@ import { Col, Container, Image, Row } from 'react-bootstrap';
 /** The Home page. */
 const Home = () => (
   <main>
-    <Container id="landing-page" fluid className="py-3">
-      <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image src="next.svg" width="150px" alt="" />
+    <Container fluid className="landing-page text-center">
+      {/* Banner */}
+      <Row className="text-center py-5" style={{ backgroundColor: "#356943", color: "white" }}>
+        <Col>
+          <h1 className="display-4 fw-bold">Fourward Thinking</h1>
+          <h6>Study Sessions at UHM</h6>
         </Col>
-
-        <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
+      </Row>
+      {/* Sign-in Section */}
+      <Row className="text-center py-4" style={({ backgroundColor: "#fff8f0"})}>
+        <Col>
+          <p className="fw-bold">Start by Signing in...</p>
+        </Col>
+      </Row>
+      { /* Example Windows */}
+      <Row className="example-windows-section" style={{ height: '45vh' }}>
+        <Col xs={6} className="d-flex justify-content-center">
+          <div className="example-window">
+            <h6>Example Window 1</h6>
+          </div>
+        </Col>
+        <Col xs={6} className="d-flex justify-content-center">
+          <div className="example-window">
+            <h6>Example Window 2</h6>
+          </div>
         </Col>
       </Row>
     </Container>
