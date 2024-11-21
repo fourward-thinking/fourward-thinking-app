73251be697791f56c3a2e8ff14e824680bc721da
Kaenaokalani Sylva <kaena.sylva@gmail.com>
2024-11-21 02:50:06 -1000

    issue-08
    modified landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,9 @@
-import { Col, Container, Row } from 'react-bootstrap';
+import LandingPage from '../components/LandingPage';
 
 /** The Home page. */
 const Home = () => (
   <main>
-    <Container fluid className="landing-page text-center">
-      {/* Banner */}
-      <Row className="text-center py-5" style={{ backgroundColor: '#356943', color: 'white' }}>
-        <Col>
-          <h1 className="display-4 fw-bold">Fourward Thinking</h1>
-          <h6>Study Sessions at UHM</h6>
-        </Col>
-      </Row>
-      {/* Sign-in Section */}
-      <Row className="text-center py-4" style={({ backgroundColor: '#fff8f0' })}>
-        <Col>
-          <p className="fw-bold">Start by Signing in...</p>
-        </Col>
-      </Row>
-      { /* Example Windows */}
-      <Row className="example-windows-section" style={{ height: '45vh' }}>
-        <Col xs={6} className="d-flex justify-content-center">
-          <div className="example-window">
-            <h6>Example Window 1</h6>
-          </div>
-        </Col>
-        <Col xs={6} className="d-flex justify-content-center">
-          <div className="example-window">
-            <h6>Example Window 2</h6>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <LandingPage />
   </main>
 );
 
---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import Image from 'next/image';
+
+const LandingPage = () => (
+  <Container fluid className="landing-page text-center p-0">
+    {/* Banner */}
+    <Row className="banner">
+      <Col>
+        <h1 className="display-4 fw-bold">Fourward Thinking</h1>
+        <h5>Find study sessions at UHM</h5>
+      </Col>
+    </Row>
+    {/* Example Windows */}
+    <Row className="example-windows-section">
+      <Col xs={6} className="d-flex justify-content-center">
+        <div className="image-window">
+          <Image
+            src="/placeholder-2.jpg"
+            alt="Placeholder"
+            fill
+            className="image-fill"
+          />
+        </div>
+      </Col>
+      <Col xs={5} className="d-flex justify-content-center">
+        <Row className="text-window">
+          <h1 className="landing-large-font">Study Better Together</h1>
+          <h3 className="landing-small-font">
+            Your ultimate hub for collaborative learning! Connect with fellow students, join or create study sessions,
+            enhance your learning experience.
+          </h3>
+          <Button className="button-one">Find Sessions...</Button>
+        </Row>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default LandingPage;
